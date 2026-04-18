720e30ab44c047fd5259fcc5944eefcbab57f2c2
Lipedev300 <luizfelipebasso22@gmail.com>
2026-04-18 15:36:06 -0300

    feat: front-end com next.js criado, página de boas-vindas do APM já criada, só falta a estilização

--- app/page.tsx
+++ app/page.tsx
@@ -1,64 +1,14 @@
-import Image from "next/image";
-
-export default function Home() {
+export default function Welcome() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
+    <div>
+      <h1> APM, gerenciamento de projetos sem barreiras</h1>
+      <main>
+        <p> Você já precisou gerenciar seu projeto, seja ele de software ou de outro tipo, e enfrentou dificuldades de navegação e experiência ruim na gestão? </p>
+        <p> Muitas ferramentas famosas nesse âmbito, como Trelo e Jira, focam mais na quantidade de funcionalidades do que na experiência do usuário em suas plataformas, colocando muita coisa em uma tela só, causando poluição visual e auditiva para quem usa </p>
+        <p> Aqui no APM, Accessible Project Manager, você nunca mais terá esse problema. </p>
+        <p> Com o APM, você gerencia seus projetos de forma fácil, intuitiva e acessível, contando com interfaces organizadas e sem distrações na tela que atrapalhem a sua experiência de gestão </p>
+        <p> <strong> Ele é construído desde o início focado em acessibilidade para usuários com deficiência, tendo alta compatibilidade com leitores de tela (NVDA e JAWS no computador e talkback/voice over em dispositivos móveis), além de auto-contraste nas informações para ajudar usuários com baixa-visão </strong></p>
+        <p> comece a gerenciar seus projetos com total facilidade! </p>
       </main>
     </div>
   );
